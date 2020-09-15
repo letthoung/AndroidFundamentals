98b9333622aa7b979c67db6e8701c2a5f998f8f7
letthoung <thanhtung3496@gmail.com>
2020-09-15 14:56:45 -0400

    Finished codelab 01.2 DiceRoller

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView;
+    lateinit var rollButton: Button;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage = dice_image;
+        rollButton = roll_button;
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+    private fun rollDice() {
+        val randomInt = (1..6).random();
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableResource)
+    }
+
+}
